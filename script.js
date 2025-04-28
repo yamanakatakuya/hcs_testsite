f15bb87978bd3ae158cdd072eb6ee5171418b48b
Takuya Yamanaka <45190867+yamanakatakuya@users.noreply.github.com>
2025-04-28 15:43:53 +0900

    initial

---
+++ script.js
@@ -0,0 +1,11 @@
+function register() {
+  alert('Register function not connected to backend in this version.');
+}
+
+function login() {
+  alert('Login function not connected to backend in this version.');
+}
+
+function submitData() {
+  alert('Submit function not connected to backend in this version.');
+}
